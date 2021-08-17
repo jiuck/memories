cb56bb91020f4d399ebf71185053e5a5bf377a03
jiuck <jiuck.grecords@gmail.com>
2021-08-17 18:34:49 +0200

    Updates

--- src/components/EditorToHTML.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
-import EditorToHTML from "./EditorToHTML";
-
-test("renders content", () => {
-  return undefined;
-});
